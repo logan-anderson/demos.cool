9482f70f9adb6ef46fcdd789574cafb3e83f3be3
Logan Anderson <landerson2@upei.ca>
2024-04-30 22:47:15 -0300

    Fix build.ts

--- framework/build.ts
+++ framework/build.ts
@@ -1,37 +1,25 @@
-import { writeFile, readFile } from "node:fs/promises";
+import { writeFile } from "node:fs/promises";
 import path from "node:path";
 import { build } from "vite";
 import { renameBuildToIndex } from "./renameBuildToIndex";
 import { renderHtmlContent } from "./render";
+import { getDemos } from "./getDemos";
 
-// import { fileURLToPath } from "node:url";
 // const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
-const buildHtmlFile = async (folder) => {
-  const p = path.join(folder, "entry-server.tsx");
-  const renderFunc = (await import(p)).render;
-  const { html } = await renderFunc();
-  const htmlTemplate = await readFile(
-    path.join(folder, "layout.html"),
-    "utf-8"
-  );
-  const htmlContent = htmlTemplate.replace("<!--app-html-->", html);
-
-  // todo make html template and then replace the html with the rendered html
-  const htmlFile = path.join(folder, "build.html");
-  await writeFile(htmlFile, htmlContent);
-};
 const buildAll = async () => {
-  const paths = ["", "demos/local-llm"];
   const root = process.cwd();
-  const folders = paths.map((f) => path.join(root, f));
-  for (const folder of folders) {
-    await buildHtmlFile(folder);
-    const htmlContent = await renderHtmlContent(folder, {
+  const demos = getDemos(root);
+  const demosWithFolder = demos.map((d) => path.join("demos", d));
+  const paths = ["", ...demosWithFolder];
+
+  for (const folder of paths) {
+    const currentFolder = path.join(root, folder);
+
+    const htmlContent = await renderHtmlContent(currentFolder, {
       dev: false,
       url: "asdf",
-      // TODO: get demos from folder
-      demos: [],
+      demos,
     });
     const htmlFile = path.join(folder, "build.html");
     await writeFile(htmlFile, htmlContent);
