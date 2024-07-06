fb78614a66558e026f2e08ba64b7b9b6f4b94338
Logan Anderson <landerson2@upei.ca>
2024-07-06 12:35:10 -0300

    use rehype

--- framework/astParser.ts
+++ framework/astParser.ts
@@ -1,12 +1,11 @@
+import { unified } from "unified";
 import { fromHtml } from "hast-util-from-html";
-import { toHtml } from "hast-util-to-html";
 import { visit } from "unist-util-visit";
+import rehypeParse from "rehype-parse";
+import rehypeStringify from "rehype-stringify";
+import type { Element } from "hast";
 
-export const insertIntoIdRoot = ({
-  outerHtml,
-  innerHtml,
-  injectScript,
-}: {
+type Options = {
   outerHtml: string;
   innerHtml: string;
   injectScript?:
@@ -14,36 +13,47 @@ export const insertIntoIdRoot = ({
         path: string;
       }
     | false;
-}) => {
-  const hast = fromHtml(outerHtml);
+};
+
+const customParser = ({ injectScript, innerHtml }: Options) => {
   const innerElArr = fromHtml(innerHtml).children.filter(
     (x) => x.type === "element"
   );
   if (innerElArr === undefined || innerElArr.length === 0) {
     throw new Error("inner element not found");
   }
+  return (tree) => {
+    visit(tree, "element", (node: Element) => {
+      if (node.tagName === "body" && injectScript) {
+        node.children.push({
+          type: "element",
+          tagName: "script",
+          properties: {
+            "data-reload": true,
+            type: "module",
+            src: injectScript.path,
+          },
+          children: [],
+        });
+      }
+      if (
+        node.tagName === "div" &&
+        node.properties.id === "root" &&
+        node.children.length === 0
+      ) {
+        node.children.push(...innerElArr);
+      }
+    });
+  };
+};
 
-  visit(hast, "element", (node) => {
-    if (node.tagName === "body" && injectScript) {
-      node.children.push({
-        type: "element",
-        tagName: "script",
-        properties: {
-          "data-reload": true,
-          type: "module",
-          src: injectScript.path,
-        },
-        children: [],
-      });
-    }
-    if (
-      node.tagName === "div" &&
-      node.properties.id === "root" &&
-      node.children.length === 0
-    ) {
-      node.children.push(...innerElArr);
-    }
-  });
+export const insertIntoIdRoot = (options: Options) => {
+  const { outerHtml } = options;
 
-  return toHtml(hast);
+  const test = unified()
+    .use(rehypeParse)
+    .use(customParser, options)
+    .use(rehypeStringify)
+    .processSync(outerHtml);
+  return String(test);
 };
