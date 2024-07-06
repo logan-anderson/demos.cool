66c1dd01b18da0da25bc79f7b5b3b4269c9c6bbb
Logan Anderson <landerson2@upei.ca>
2024-07-06 15:54:29 -0300

    remove unused files

--- rootLayout.tsx
+++ rootLayout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ demos }: { demos: string[] }) => {
       <head>
         <meta charSet="UTF-8" />
         <link rel="stylesheet" href="/src/global.css" />
-        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Default Layout</title>
         <script defer type="module" src="/src/router.ts"></script>
