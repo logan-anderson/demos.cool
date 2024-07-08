51767f8c8820a8657b23a7273a78413fabfeb3d9
Logan Anderson <landerson2@upei.ca>
2024-07-07 22:51:06 -0300

    update content

--- src/home/homePage.tsx
+++ src/home/homePage.tsx
@@ -2,18 +2,19 @@ export const HomePage = () => {
   return (
     <main>
       <link rel="stylesheet" href="/src/home/homePage.css" />
-      <div className="text-center">
+      <div className="text-center max-w-4xl mx-auto">
         <h1 className="text-6xl my-3">
           <span className="wave">👋</span> Welcome to demos.cool
         </h1>
         <p className="text-2xl my-3">
           This is my personal collection of
           <strong className="text-brand-50 font-extrabold"> cool </strong>demos
-          all built with my own homebrewed framework.
+          all built with my own homebrewed framework that handles SSG and SSR in
+          any framework.
         </p>
         <div className="my-7">
           <a className="p-2 rounded-full bg-brand-50" href="/demos">
-            <span className="bg-gradient-to-r hover:text-brand-400  from-blue-500  to-brand-400 text-transparent bg-clip-text font-bold py-2 px-4 rounded-full transition-colors">
+            <span className="bg-gradient-to-r hover:text-brand-400  from-blue-700  to-brand-400 text-transparent bg-clip-text font-bold py-2 px-4 rounded-full transition-colors">
               View Demos
             </span>
           </a>
